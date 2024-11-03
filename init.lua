e2afb6ded27e848fd8d7dfd488246c8e55689fb9
Daniel Torres <danieltorres2902@gmail.com>
2024-11-03 12:19:23 -0300

    updated init file

--- init.lua
+++ init.lua
@@ -1,2 +1,103 @@
-require("settings")
-require("lazy-setup")
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- relative number lines
+vim.opt.relativenumber = true
+
+-- Enable mouse mode
+-- vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.keymap.set('n', '<leader>m', vim.cmd.Vexplore)
+vim.keymap.set('n', '<C-n>', vim.cmd.tabnew)
+vim.keymap.set('n', '<C-x>', vim.cmd.sp)
+vim.keymap.set('n', '<C-l>', vim.cmd.vsp)
+vim.keymap.set('n', '<leader><Left>', vim.cmd.bprevious)
+vim.keymap.set('n', '<leader><Right>', vim.cmd.bnext)
+
+local opts = { noremap = true, silent = true }
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set('v', '<', '<gv^', opts)
+vim.keymap.set('v', '>', '>gv^', opts)
+
+-- Move text up and down
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set('v', 'p', '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+vim.keymap.set('x', 'J', ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set('x', 'K', ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set('x', '<A-j>', ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set('x', '<A-k>', ":m '<-2<CR>gv=gv", opts)
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+require 'lazy-setup'
